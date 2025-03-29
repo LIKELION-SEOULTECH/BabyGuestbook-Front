451cfdc55557caf4e4fefdbb7955759bfecaaa12
Dohyeop Lim <dhlim0817@gmail.com>
2025-03-30 02:41:38 +0900

    feat: implement GuestbookList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import GuestbookList from "./components/guestbook/GuestbookList";
 import Header from "./components/layout/Header";
+import { mockGuestbookItems } from "./constants/mockData";
 
 function App() {
     return (
-        <div className="w-full max-w-4xl mx-auto px-4 pt-16 pb-12">
+        <div className="max-w-4xl mx-auto px-4 pt-16 pb-12">
             <Header />
+
+            <div className="h-16"></div>
+
+            <GuestbookList items={mockGuestbookItems} />
         </div>
     );
 }
