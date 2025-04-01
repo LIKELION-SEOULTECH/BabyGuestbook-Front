808ba026a6fe1cb0e7057ca0e68815692862b17b
Dohyeop Lim <dhlim0817@gmail.com>
2025-04-01 19:45:49 +0900

    feat: enhance post API methods

--- src/api/post.ts
+++ src/api/post.ts
@@ -6,11 +6,19 @@ import type {
     UpdatePostRequest,
     DeletePostRequest,
     ReadPostParameter,
+    PostDTO,
 } from "@/types/post";
+
+interface ApiResponse<T> {
+    code: string;
+    statusCode: number;
+    message: string;
+    data: T;
+}
 
 // ● GET: 방명록 목록 조회 (초기 로딩)
 export const fetchPosts = async (
-    params: Omit<ReadPostParameter, "lastPostId"> // 두 번째 req부터 lastPostId 필요
+    params: Omit<ReadPostParameter, "lastPostId">
 ): Promise<PostResponse> => {
     const response = await client.get<PostResponse>("/api/v1/posts", {
         params,
@@ -29,23 +37,40 @@ export const fetchInfinitePosts = async (
 };
 
 // ● POST: 방명록 작성
-export const createPost = async (body: CreatePostRequest) => {
-    const response = await client.post("/api/v1/posts/post", body);
+export const createPost = async (
+    body: CreatePostRequest
+): Promise<ApiResponse<PostDTO>> => {
+    const response = await client.post<ApiResponse<PostDTO>>(
+        "/api/v1/posts/post",
+        body
+    );
     return response.data;
 };
 
 // ● PATCH: 방명록 수정
-export const updatePost = async (postId: number, body: UpdatePostRequest) => {
-    const response = await client.patch(`/api/v1/posts/${postId}`, body);
+export const updatePost = async (
+    postId: number,
+    body: UpdatePostRequest
+): Promise<ApiResponse<PostDTO>> => {
+    const response = await client.patch<ApiResponse<PostDTO>>(
+        `/api/v1/posts/${postId}`,
+        body
+    );
     return response.data;
 };
 
 // ● DELETE: 방명록 삭제
-export const deletePost = async (postId: number, body: DeletePostRequest) => {
-    const response = await client.delete(`/api/v1/posts/${postId}`, {
-        params: {
-            password: body.password,
-        },
-    });
+export const deletePost = async (
+    postId: number,
+    body: DeletePostRequest
+): Promise<ApiResponse<null>> => {
+    const response = await client.delete<ApiResponse<null>>(
+        `/api/v1/posts/${postId}`,
+        {
+            params: {
+                password: body.password,
+            },
+        }
+    );
     return response.data;
 };
